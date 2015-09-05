55910810812aa31deebec1ccf5ba2c0c2eca7774
Tremens8 <lucasemanuelgonzalez8@gmail.com>
2015-09-05 06:01:48 -0300

    Proyect created

---
+++ CSharp Game Engine/Console.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Game_Engine
+{
+    public partial class Console : Form
+    {
+        static public Console Instance;
+        public Console(Form parent)
+        {
+            Instance = this;
+            InitializeComponent();
+            this.Location = new Point(parent.Location.X, parent.Location.Y + parent.Height);
+            this.Size = new Size(parent.Width, this.Size.Height);
+        }
+        static public void SendMessage(string message, object sender)
+        {
+            if (Instance != null)
+            {
+                Instance.consoleLog.Invoke((MethodInvoker)delegate
+                {
+                    Instance.consoleLog.AppendText(message+"\n");
+                });
+            }
+        }
+        static public void ClearConsole()
+        {
+            if (Instance != null)
+            {
+                Instance.consoleLog.Invoke((MethodInvoker)delegate
+                {
+                    Instance.consoleLog.Text = "";
+                });
+            }
+        }
+        private void consoleSendBtn_Click(object sender, EventArgs e)
+        {
+            if (consoleTextBox.Text != "")
+                consoleLog.AppendText(consoleTextBox.Text+"\n");
+        }
+        private void Console_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ CSharp Game Engine/Game.cs
@@ -0,0 +1,58 @@
+﻿using CSharp_Game_Engine.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Game_Engine
+{
+    public struct Sprite
+    {
+        public Bitmap image;
+        public Vector position;
+
+        public Sprite(Bitmap _image, Vector _point)
+        {
+            image = _image;
+            position = _point;
+        }
+    }
+    public class Game : EngineCore
+    {
+
+        private Sprite spt;
+        public Game()
+        {
+            spt.image = Resources.hitler;
+            spt.position = new Vector(0, GraphicsCore.Instance.buffer.Graphics.VisibleClipBounds.Height/2);
+            Start();
+            Vector v = new Vector(75,1.5f) + new Vector(100,1.5f);
+        }
+
+        public override void Update()
+        {
+            spt.position.X += 0.1f;
+            //spt.position.Y = (float)Math.Cos((double)spt.position.X);
+        }
+        public override void Render()
+        {
+            GraphicsCore.Instance.buffer.Graphics.Clear(Color.Coral);
+            GraphicsCore.Instance.DrawBitMap(spt.image, spt.position);
+            GraphicsCore.Instance.buffer.Render();
+        }
+
+        private Color GerRandomColor()
+        {
+            Random randomGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randomColorName = names[randomGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randomColorName);
+            return randomColor;
+
+        }
+    }
+}
